41c032ecca3c863698ec5b4161d14daf14b92166
tatvavid <tatvavid@gmail.com>
2021-11-16 12:25:57 +0530

    commit

---
+++ lib/screens/view_product/components/body.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
