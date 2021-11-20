a585822bafe3003506aade2e10125fd9cab93a66
tatvavid <tatvavid@gmail.com>
2021-11-20 20:33:41 +0530

    commit

--- lib/screens/sign_in/components/sign_form.dart
+++ lib/screens/sign_in/components/sign_form.dart
@@ -4,6 +4,7 @@ import 'package:shop_app/components/form_error.dart';
 import 'package:shop_app/helper/keyboard.dart';
 import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
 import 'package:shop_app/screens/login_success/login_success_screen.dart';
+import 'package:shop_app/services/auth.dart';
 
 import '../../../components/default_button.dart';
 import '../../../constants.dart';
@@ -15,6 +16,7 @@ class SignForm extends StatefulWidget {
 }
 
 class _SignFormState extends State<SignForm> {
+  final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
   String? email;
   String? password;
@@ -72,13 +74,23 @@ class _SignFormState extends State<SignForm> {
           SizedBox(height: getProportionateScreenHeight(20)),
           DefaultButton(
             text: "Continue",
-            press: () {
-              if (_formKey.currentState!.validate()) {
-                _formKey.currentState!.save();
-                // if all are valid then go to success screen
-                KeyboardUtil.hideKeyboard(context);
-                Navigator.pushNamed(context, LoginSuccessScreen.routeName);
+            press: () async {
+              dynamic result = await _auth.signInAnon();
+              if (result==null){
+                print('Error Signing In ');
               }
+              else {
+                print('Signed in');
+              }
+
+
+
+              // if (_formKey.currentState!.validate()) {
+              //   _formKey.currentState!.save();
+              //   // if all are valid then go to success screen
+              //   KeyboardUtil.hideKeyboard(context);
+              //   Navigator.pushNamed(context, LoginSuccessScreen.routeName);
+              // }
             },
           ),
         ],
