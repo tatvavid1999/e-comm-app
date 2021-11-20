24c9760fdb58d39cc1d3f90c89a4961feec2c267
tatvavid <tatvavid@gmail.com>
2021-11-20 21:00:21 +0530

    commit

---
+++ lib/services/auth.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user onject
+
+
+
+  //sign in anon
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+      return user;
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+
+  }
+
+
+//sign in email
+
+
+//register new
+
+
+//signout
+}
