3a4324caaf754616d46b72461c06a5fdd52a8ec5
tatvavid <tatvavid@gmail.com>
2021-11-23 14:04:16 +0530

    commit

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,8 +3,30 @@ class UserModel{
 
   String? uid;
   String? email;
+  String? type;
+  String? phoneNumber;
 
 
 
-  UserModel({this.uid, this.email, this.name });
+  UserModel({this.uid, this.email, this.name, this.phoneNumber, this.type});
+  factory UserModel.fromMap(map) {
+    return UserModel(
+      uid: map['uid'],
+      email: map['email'],
+      name: map['name'],
+      phoneNumber: map['phoneNumber'],
+      type: map['type']
+    );
+  }
+
+  // sending data to our server
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'name': name,
+      'phoneNumber': phoneNumber,
+      'type': type,
+    };
+  }
 }
--- lib/screens/sign_in/components/sign_form.dart
+++ lib/screens/sign_in/components/sign_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:shop_app/components/custom_surfix_icon.dart';
 import 'package:shop_app/components/form_error.dart';
 import 'package:shop_app/helper/keyboard.dart';
+import 'package:shop_app/models/user_model.dart';
 import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
 import 'package:shop_app/screens/home/home_screen.dart';
 import 'package:shop_app/screens/login_success/login_success_screen.dart';
@@ -186,6 +187,7 @@ class _SignFormState extends State<SignForm> {
       .then((uid) => {
         print('form validated'),
         Fluttertoast.showToast(msg: 'Login Successful'),
+
         Navigator.pushNamed(context, HomeScreen.routeName)
       });
 
